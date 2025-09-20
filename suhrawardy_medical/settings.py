9363f449f6f998fceada500c0fae6630b27e17dd
Walid Ibne Hasan <walid.ibne.hasan@gmail.com>
2025-09-20 10:35:50 +0600

    settings JWT lifetime updated

--- suhrawardy_medical/settings.py
+++ suhrawardy_medical/settings.py
@@ -98,8 +98,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=120),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=180),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
 }
