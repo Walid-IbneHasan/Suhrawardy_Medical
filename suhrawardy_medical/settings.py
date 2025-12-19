477fb8f4ab5375d4bd42a9541a9a1dfcca02a205
Walid Ibne Hasan <walid.ibne.hasan@gmail.com>
2025-12-19 11:41:13 +0600

    CORS ALLOWED ORIGIN ALL SET TO FALSE

--- suhrawardy_medical/settings.py
+++ suhrawardy_medical/settings.py
@@ -56,13 +56,13 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "https://sandhanishsmcu.com",
     "https://www.sandhanishsmcu.com",
-    "http://localhost:8080",  # development server
+    "http://localhost:8080",
     "http://127.0.0.1:8080",
-    # Add your production Next.js domain
 ]
-CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ALLOW_ALL_ORIGINS = False
+
 
 ROOT_URLCONF = "suhrawardy_medical.urls"
 
